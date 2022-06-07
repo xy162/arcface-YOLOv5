f86f71869a1a49e885685d78dbf02be129d6d48c
xy162 <73573604+xy162@users.noreply.github.com>
2022-06-07 19:04:29 +0800

    Add files via upload

---
+++ ttt.py
@@ -0,0 +1,9 @@
+# ------------      -------    --------    -----------    -----------
+# @File       :ttt.py    
+# @Modify Time:2022/5/24 20:11
+# @Author     :董海斌
+# ------------      -------    --------    -----------    -----------
+import numpy as np
+ac = np.load('./person/dhb.npy')
+print(ac.shape)
+print(ac)
